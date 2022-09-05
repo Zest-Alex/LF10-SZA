ae696f523bf6254f1ef5758d7236b0c13bfab0ee
Zest | Alex <a.tannenberg@outlook.com>
2022-09-05 17:06:10 +0200

    changed variables name

--- Main.ps1
+++ Main.ps1
@@ -10,8 +10,8 @@ $rgname = "LF10RG01"
 $vnetname = "LF10VNET01"
 $subnetname = "LF10SUBNET01"
 $vmname = "LF10VM01"
-$segrname = "LF10SEGR01"
-$puipname = "LF10PUIP01"
+$securitygroupname = "LF10SEGR01"
+$publicipname = "LF10PUIP01"
 
 
 
@@ -23,15 +23,15 @@ New-AzResourceGroup -Name $rgname -Location $location
 $subnet = New-AzVirtualNetworkSubnetConfig -Name $subnetname -AddressPrefix "10.0.0.0/24"
 New-AzVirtualNetwork -Name $vnetname -ResourceGroupName $rgname -Location $location -AddressPrefix "10.0.0.0/16" -Subnet $subnet
 
-#Create VM 01
+#Create VM
 New-AzVm `
     -ResourceGroupName $rgname `
     -Name $vmname `
     -Location $location `
     -VirtualNetworkName $vnetname `
     -SubnetName $subnetname `
-    -SecurityGroupName $segrname `
-    -PublicIpAddressName $puipname `
+    -SecurityGroupName $securitygroupname `
+    -PublicIpAddressName $publicipname `
     -OpenPorts 80,3389
 
 #Install Webserver
